524eaefbe759b66e3d37542d927e957d7541fa82
pmleconte <pascal.leconte@conseilgouz.com>
2023-03-25 10:37:47 +0100

    2.0.2
    Initialize sent to null in automsg table

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `#__automsg` (
 `state` integer NOT NULL default 0,
 `article_id` integer NOT NULL ,
 `created` datetime NOT NULL DEFAULT '1980-01-01 00:00:00',
-`sent` datetime NOT NULL DEFAULT '1980-01-01 00:00:00',
+`sent` datetime NULL,
 PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Auto Message Table';
